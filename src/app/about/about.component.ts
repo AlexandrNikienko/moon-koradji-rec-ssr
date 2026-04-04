ca6fe681f8d8dec0aed0c8c68d02d04f5e0e7e01
Oleksandr Nikiienko <oleksandr.nikiienko@innervate.com>
2026-04-04 17:05:50 +0300

    fix

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { iMeta, MetaDataService } from './../core/services/meta-data.service';
+import { MetaDataService } from './../core/services/meta-data.service';
 import { HeadingComponent } from "../layout/heading/heading.component";
 import { JsonLdService } from '../core/services/json-ld.service';
 
@@ -14,17 +14,15 @@ export class AboutComponent {
 	private metaData = inject(MetaDataService);
 	private jsonLd = inject(JsonLdService);
 
-	private metaDataObj: iMeta = {
-		title: 'Our Mission at Moon Koradji Records',
-		description: 'Independent Ukrainian psytrance label founded in 2007 by Oleksandr Nikiienko, aka DJ Omsun.',
-		ogTitle: 'Moon Koradji Records - World Wide Psychedelic',
-		ogImage: 'https://www.moonkoradji.com/assets/images/mk_square.jpg',
-		ogUrl: 'https://www.moonkoradji.com/about',
-		ogDescription: 'Independent Ukrainian psytrance label founded in 2007 by Oleksandr Nikiienko, aka DJ Omsun.'
-	}
-
 	constructor() {
-		this.metaData.setMetaData(this.metaDataObj);
+		this.metaData.setMetaData({
+			title: 'Our Mission at Moon Koradji Records',
+			description: 'Independent Ukrainian psytrance label founded in 2007 by Oleksandr Nikiienko, aka DJ Omsun.',
+			ogTitle: 'Moon Koradji Records - World Wide Psychedelic',
+			ogImage: 'https://www.moonkoradji.com/assets/images/mk_square.jpg',
+			ogUrl: 'https://www.moonkoradji.com/about',
+			ogDescription: 'Independent Ukrainian psytrance label founded in 2007 by Oleksandr Nikiienko, aka DJ Omsun.'
+		});
 
 		this.jsonLd.setJsonLd({
 			'@context': 'https://schema.org',
