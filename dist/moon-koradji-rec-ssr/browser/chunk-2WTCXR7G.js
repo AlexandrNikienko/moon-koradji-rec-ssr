02269a06ed03902f1ea934b182cb3c3ec2b325e3
Oleksandr Nikiienko <oleksandr.nikiienko@innervate.com>
2026-04-27 13:44:51 +0300

    build

---
+++ dist/moon-koradji-rec-ssr/browser/chunk-2WTCXR7G.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-7B6PPNVJ.js";import"./chunk-TTOFHISI.js";export{a as SafeHtmlPipe};
--- dist/moon-koradji-rec-ssr/browser/chunk-VNDCINAW.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-VYSOBXSG.js";import"./chunk-2IAJYUKW.js";export{a as SafeHtmlPipe};
