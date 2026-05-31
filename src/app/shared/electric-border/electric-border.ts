c5874a42de90100ac26e824d989295fb71bb1d48
Omsun <moonkoradji@gmail.com>
2026-05-31 17:57:08 +0300

    remove hero anim

--- src/app/shared/electric-border/electric-border.ts
+++ src/app/shared/electric-border/electric-border.ts
@@ -6,39 +6,39 @@ import { afterNextRender, Component, HostBinding, signal } from '@angular/core';
     styleUrls: ['./electric-border.scss']
 })
 export class ElectricBorderComponent {
-	filterEnabled = signal(false);
+	// filterEnabled = signal(false);
 
-	constructor() {
-		afterNextRender(() => {
-			this.checkPerformance();
-		});
-	}
+	// constructor() {
+	// 	afterNextRender(() => {
+	// 		this.checkPerformance();
+	// 	});
+	// }
 
-	@HostBinding('class.is-safari')
-	public readonly isSafari = typeof window !== 'undefined' && /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+	// @HostBinding('class.is-safari')
+	// public readonly isSafari = typeof window !== 'undefined' && /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
 
-	private checkPerformance() {
-		if (window.matchMedia('(prefers-reduced-motion: reduce)').matches) return;
+	// private checkPerformance() {
+	// 	if (window.matchMedia('(prefers-reduced-motion: reduce)').matches) return;
 
-		let frames = 0;
-		let slowFrames = 0;
-		const t = { prev: performance.now() };
+	// 	let frames = 0;
+	// 	let slowFrames = 0;
+	// 	const t = { prev: performance.now() };
 
-		const measure = () => {
-			requestAnimationFrame(() => {
-				const now = performance.now();
-				if (now - t.prev > 32) slowFrames++;
-				t.prev = now;
-				frames++;
+	// 	const measure = () => {
+	// 		requestAnimationFrame(() => {
+	// 			const now = performance.now();
+	// 			if (now - t.prev > 32) slowFrames++;
+	// 			t.prev = now;
+	// 			frames++;
 
-				if (frames < 10) {
-					measure();
-				} else {
-					this.filterEnabled.set(slowFrames < 3);
-				}
-			});
-		};
+	// 			if (frames < 10) {
+	// 				measure();
+	// 			} else {
+	// 				this.filterEnabled.set(slowFrames < 3);
+	// 			}
+	// 		});
+	// 	};
 
-		measure();
-	}
+	// 	measure();
+	// }
 }
