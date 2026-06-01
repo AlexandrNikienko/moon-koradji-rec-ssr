a85764c77470accf5c8b82acbdbe738d873e1c58
Oleksandr Nikiienko <oleksandr.nikiienko@innervate.com>
2026-06-01 16:58:21 +0300

    refactoring

--- src/app/shared/electric-border/electric-border.ts
+++ src/app/shared/electric-border/electric-border.ts
@@ -1,4 +1,4 @@
-import { afterNextRender, Component, HostBinding, signal } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
 	selector: 'app-shared-electric-border',
@@ -6,39 +6,5 @@ import { afterNextRender, Component, HostBinding, signal } from '@angular/core';
     styleUrls: ['./electric-border.scss']
 })
 export class ElectricBorderComponent {
-	// filterEnabled = signal(false);
-
-	// constructor() {
-	// 	afterNextRender(() => {
-	// 		this.checkPerformance();
-	// 	});
-	// }
-
-	// @HostBinding('class.is-safari')
-	// public readonly isSafari = typeof window !== 'undefined' && /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
-
-	// private checkPerformance() {
-	// 	if (window.matchMedia('(prefers-reduced-motion: reduce)').matches) return;
-
-	// 	let frames = 0;
-	// 	let slowFrames = 0;
-	// 	const t = { prev: performance.now() };
-
-	// 	const measure = () => {
-	// 		requestAnimationFrame(() => {
-	// 			const now = performance.now();
-	// 			if (now - t.prev > 32) slowFrames++;
-	// 			t.prev = now;
-	// 			frames++;
-
-	// 			if (frames < 10) {
-	// 				measure();
-	// 			} else {
-	// 				this.filterEnabled.set(slowFrames < 3);
-	// 			}
-	// 		});
-	// 	};
-
-	// 	measure();
-	// }
+	
 }
